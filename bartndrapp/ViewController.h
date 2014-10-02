aee89daf65ff46e460fd0ea25564e218c89bb5d5
Peter Kim <peterkim45366@gmail.com>
2014-10-01 20:04:48 -0700

    Initial Commit

---
+++ bartndrapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bartndrapp
+//
+//  Created by Peter Kim on 10/1/14.
+//  Copyright (c) 2014 Bartdndr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ bartndrapp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  bartndrapp
+//
+//  Created by Peter Kim on 10/1/14.
+//  Copyright (c) 2014 Bartdndr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
